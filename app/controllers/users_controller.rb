f7abb6255309f4ec4ccada63541352484438b70f
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-10-27 08:58:15 -0700

    made most of functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     @user = User.all 
   end 
-  
+
   def new
     @user = User.new
   end
@@ -11,11 +11,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You have successfully signed up!"
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :index, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   # Create a better looking URL for logging in
   get '/login', to: 'sessions#new'
