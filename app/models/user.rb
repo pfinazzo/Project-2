979c1147451da410f27f0673b6458d6fc9511a6e
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-11-02 11:50:20 -0700

    seed fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :lessons
+  has_many :lessons, dependent: :destroy
   has_many :lessons_attending, class_name: 'Lesson', foreign_key: :student_id
   has_attached_file :image, styles: { medium: "500x500", thumb: "250x250"}, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
