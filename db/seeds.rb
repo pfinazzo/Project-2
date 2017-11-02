1fdc6b3392d20475f1ff33f10a3c0b037abda682
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-11-02 11:48:09 -0700

    fixed seeds destroy_all order

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
 Lesson.destroy_all
+User.destroy_all
+
 
 
